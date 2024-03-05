b62e96016b1a84e395cee6e8a75b6c4394c7db78
Matthew Sweet <sweet@andrews.edu>
2024-03-05 14:50:12 -0600

    Updated state hooks for adding items

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,40 @@ interface TodoItem {
 	date: string;
 }
 
+interface TodoList extends Array<TodoItem> {}
+
+let nextId = 0;
+
 function App() {
-	const [todo, setTodo] = useState([
-		{ id: 1, todo: "Hello, world!", date: "03/04/24" },
-		{ id: 2, todo: "Hello, again!", date: "03/04/24" },
-	]);
+	const [todo, setTodo] = useState<TodoList>([]);
+	const [newTodo, setNewTodo] = useState("");
+
+	function handleNewTodoEvent(data: TodoItem) {
+		setTodo([...todo, data]);
+		setNewTodo("");
+	}
 
 	return (
 		<div className="container">
 			<div className="todo-list">
 				<h1>React Todo List</h1>
+				<input
+					type="text"
+					placeholder="New todo"
+					value={newTodo}
+					onChange={(e) => setNewTodo(e.target.value)}
+				/>
+				<button
+					onClick={() =>
+						handleNewTodoEvent({
+							id: nextId++,
+							todo: newTodo,
+							date: "03/05/24",
+						})
+					}
+				>
+					Add item
+				</button>
 				<ul>
 					{todo.map((item: TodoItem) => {
 						return <li key={item.id}>{item.todo}</li>;
